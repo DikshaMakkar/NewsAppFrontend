7fca8f6c966c3655e753b9daa8e9fc2264ac957c
Diksha <makkardiksha02@gmail.com>
2024-07-09 01:13:11 +0530

    Create GetTimesNewsArticles.js

---
+++ src/fetchAPI/GetTimesNewsArticles.js
@@ -0,0 +1,14 @@
+export const GetTimesNewsArticles = async ({searchKeyword}) => {
+    try{
+        const response = await fetch(`http://localhost:8080/api/news/guardian-news?searchKeyword=${searchKeyword}&page=1`);
+        if(!response.ok){
+            throw new Error("Http error!");
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        // throw new Error(`Error! ${error}`);
+        return [];
+    }
+}
