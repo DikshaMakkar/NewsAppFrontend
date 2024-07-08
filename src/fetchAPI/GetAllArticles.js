2ff611f22a1ad6aa9b2216023ca17a1d25e5d125
Diksha <makkardiksha02@gmail.com>
2024-07-09 01:12:49 +0530

    Create GetAllArticles.js

---
+++ src/fetchAPI/GetAllArticles.js
@@ -0,0 +1,14 @@
+export const GetAllArticles = async ({searchKeyword}) => {
+    try{
+        const response = await fetch(`http://localhost:8080/api/news?searchKeyword=${searchKeyword}&page=1`);
+        if(!response.ok){
+            throw new Error("Http error!");
+        }
+        const data = await response.json();
+        return data;
+    }
+    catch(error){
+        // throw new Error(`Error! ${error}`);
+        return [];
+    }
+}
